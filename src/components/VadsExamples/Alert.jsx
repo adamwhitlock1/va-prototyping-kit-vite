8dfa44f3b01a3a34bf6a01f2aeaba832f343b546
Adam Whitlock <8332986+adamwhitlock1@users.noreply.github.com>
2024-12-18 00:05:30 -0700

    add headline slot to alert example

--- src/components/VadsExamples/Alert.jsx
+++ src/components/VadsExamples/Alert.jsx
@@ -4,7 +4,8 @@ export const VadsAlert = () => {
       <div className="grid-col-12">
         <h3 className="vads-u-font-weight--bold">Alert</h3>
         <va-alert>
-          Alert - Content
+          <h4 slot="headline">Alert Title</h4>
+          <p>Alert Content</p>
         </va-alert>
       </div>
     </div>
